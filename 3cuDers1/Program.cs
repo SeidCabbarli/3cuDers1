7f01ce8c1f599ef767743a3910077368a94feab2
Baku <Baku@SEID>
2024-11-28 19:43:02 +0400

    Add project files.

---
+++ 3cuDers1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _3cuDers1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i =0; i <=10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
